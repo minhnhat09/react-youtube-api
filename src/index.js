f0cbbbdb87580015c3caf3f257a41f6d14db7604
NGUYEN Nhat Minh <nath-minh.nguyen_externe@group-sma.fr>
2017-04-28 18:05:20 +0200

    implement video list

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 
 const API_KEY = 'AIzaSyDRSKyBw3TbaenthoJlfAWAwq9Wm3TvOSU';
-YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
-  console.log(data);
-});
 
-const App = () => {
-  return <div>Hi</div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (data) => {
+      console.log(data);
+      this.setState({ videos: data });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
